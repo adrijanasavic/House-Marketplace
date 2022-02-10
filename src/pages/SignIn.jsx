9114d0c7513181b34bea3706f824feacf833f36e
Adrijana <adrijana.jovicic@gmail.com>
2022-02-10 20:06:30 +0100

    Google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { toast } from 'react-toastify'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import OAuth from '../components/OAuth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibillityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -96,7 +97,7 @@ function SingIn() {
               </button>
             </div>
           </form>
-          {/* Google OAuth */}
+          <OAuth/>
           <Link to='/sign-up' className='registerLink'>
             Sign Up Instead
           </Link>
