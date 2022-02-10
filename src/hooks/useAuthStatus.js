436abafd75c1333f4dcf9988d0cf65260e6f27d2
Adrijana <adrijana.jovicic@gmail.com>
2022-02-10 19:27:05 +0100

    PrivateRoute Component & useAuthStatus Hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,31 @@
+import { useEffect, useState, useRef  } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+export const useAuthStatus = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [chekingStatus, setChekingStatus] = useState(true)
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+        if (isMounted) {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setLoggedIn(true)
+                }
+                setChekingStatus(false)
+            })
+        }
+
+        return () => {
+            isMounted.current = false
+        }
+    }, [isMounted])
+    return { loggedIn, chekingStatus }
+}
+
+
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
