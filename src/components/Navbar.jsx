f5a7296ff201e7aa54f3e8597404aa0002e11fdd
Adrijana <adrijana.jovicic@gmail.com>
2022-02-11 13:25:23 +0100

    Offers Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
                                 ? 'navbarListItemNameActive'
                                 : 'navbarListItemName'}
                         >
-                            Offer
+                            Offers
                         </p>
                     </li>
                     <li className="navbarListItem" onClick={() => navigate('/profile')}>
