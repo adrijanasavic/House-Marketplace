e641a8759a2a2421e7580c9c8bd55af9d7ac9eaa
Adrijana <adrijana.jovicic@gmail.com>
2022-02-14 21:35:21 +0100

    Listings Page Slider

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -15,7 +15,7 @@ import Spinner from '../components/Spinner'
 
 function CreateListing() {
   // eslint-disable-next-line
-  const [geolocationEnabled, setGeolocationEnabled] = useState(false)
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
   const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     type: 'rent',
@@ -113,7 +113,7 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
+      // location = address
     }
 
     // Store image in firebase
